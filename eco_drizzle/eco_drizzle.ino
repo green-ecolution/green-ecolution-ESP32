c686534e4376c5136a8d46d4d5fe157d7390b1a2
NiklasHargarter <niklashargarter@gmail.com>
2025-02-23 20:40:11 +0100

    Add refactored lorawan sketch with gps

---
+++ eco_drizzle/eco_drizzle.ino
@@ -0,0 +1,139 @@
+#include "heltec.h"
+#include "LoRaWan_APP.h"
+#include "secrets.h"
+#include "helpers.h"
+#include "gps.h"
+
+#define GPSTXD 33  // Connected to RX pin of GPS module
+#define GPSRXD 34  // Connected to TX pin of GPS module
+
+/* OTAA parameters are inside secrets.h */
+
+/* ABP para need to be kept */
+uint8_t nwkSKey[] = {};
+uint8_t appSKey[] = {};
+uint32_t devAddr = (uint32_t)0x00000000;
+/* LoRaWAN channels mask, default channels 0-7 */
+uint16_t userChannelsMask[6] = { 0x00FF, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
+
+/* LoRaWAN region, select in Arduino IDE tools */
+LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
+
+/* LoRaWAN Class, Class A and Class C are supported */
+DeviceClass_t loraWanClass = CLASS_A;
+
+/* Application data transmission duty cycle in milliseconds */
+uint32_t appTxDutyCycle = 15000;
+
+/* OTAA or ABP */
+bool overTheAirActivation = true;
+
+/* ADR enable */
+bool loraWanAdr = true;
+
+/* Indicates if the node is sending confirmed or unconfirmed messages */
+bool isTxConfirmed = true;
+
+/* Application port */
+uint8_t appPort = 2;
+uint8_t confirmedNbTrials = 4;
+
+const uint32_t scaleFactor = 1000000;  // used for float scaling, keeps 6 decimal places
+
+// Variables to store GPS data
+float latitude = 0.0;
+float longitude = 0.0;
+uint32_t timeTaken = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+  Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
+
+  // Print the device name to the Serial Monitor
+  Serial.print("Device Name: ");
+  Serial.println(deviceName);
+  // Initialize RS485 communication for SMT100 sensor
+  Serial1.begin(9600);
+  initGPS(GPSRXD, GPSTXD);
+
+  Serial.println("Setup complete. Starting LoRaWAN state machine...");
+}
+
+void loop() {
+  switch (deviceState) {
+    case DEVICE_STATE_INIT:
+      {
+        Serial.println("State: DEVICE_STATE_INIT - Initializing LoRaWAN...");
+
+#if (LORAWAN_DEVEUI_AUTO)
+        LoRaWAN.generateDeveuiByChipID();  // Generate DevEUI from chip ID (if supported)
+#endif
+
+        LoRaWAN.init(loraWanClass, loraWanRegion);
+        LoRaWAN.setDefaultDR(3);  // Set default data rate
+
+        deviceState = DEVICE_STATE_JOIN;  // Move to the JOIN state
+        break;
+      }
+    case DEVICE_STATE_JOIN:
+      {
+        Serial.println("State: DEVICE_STATE_JOIN - Attempting to join the network...");
+        LoRaWAN.join();
+        break;
+      }
+    case DEVICE_STATE_SEND:
+      {
+        Serial.println("State: DEVICE_STATE_SEND - Preparing and sending data...");
+        prepareTxFrame(appPort);
+        LoRaWAN.send();
+        deviceState = DEVICE_STATE_CYCLE;  // Move to the CYCLE state
+        break;
+      }
+    case DEVICE_STATE_CYCLE:
+      {
+        Serial.println("State: DEVICE_STATE_CYCLE - Scheduling next transmission...");
+        txDutyCycleTime = appTxDutyCycle + randr(-APP_TX_DUTYCYCLE_RND, APP_TX_DUTYCYCLE_RND);
+        LoRaWAN.cycle(txDutyCycleTime);
+        deviceState = DEVICE_STATE_SLEEP;  // Move to the SLEEP state
+        break;
+      }
+    case DEVICE_STATE_SLEEP:
+      {
+        LoRaWAN.sleep(loraWanClass);
+        break;
+      }
+    default:
+      {
+        Serial.println("State: UNKNOWN - Resetting to DEVICE_STATE_INIT...");
+        deviceState = DEVICE_STATE_INIT;  // Reset to INIT state
+        break;
+      }
+  }
+}
+
+void prepareTxFrame(uint8_t port) {
+
+  Serial.println("Turn ON Vext");
+  Heltec.VextON();
+  delay(500);  // maybe unnecessary
+
+  float temp = 25.5;
+  float waterContent = 33.3;
+
+  getGPSSignal(latitude, longitude, timeTaken);
+
+  // Prepare the payload
+  appDataSize = 0;
+
+  // Add floats to the payload
+  addFloatToPayload(appData, appDataSize, temp);  // No scaling
+  addFloatToPayload(appData, appDataSize, waterContent, scaleFactor);
+  addFloatToPayload(appData, appDataSize, latitude, scaleFactor);   // Scaled
+  addFloatToPayload(appData, appDataSize, longitude, scaleFactor);  // Scaled
+  addLongToPayload(appData, appDataSize, timeTaken);
+
+  Serial.println("Payload prepared with temp, watercontent, lat, lng.");
+  Serial.println("Turn OFF Vext");
+  Heltec.VextOFF();
+}
---
+++ eco_drizzle/helpers.h
@@ -0,0 +1,10 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <Arduino.h>
+
+// Declare helper functions
+void addFloatToPayload(uint8_t* appData, uint8_t& appDataSize, float value, uint32_t scaleFactor = 0);
+void addLongToPayload(uint8_t* appData, uint8_t& appDataSize, uint32_t value);
+
+#endif
